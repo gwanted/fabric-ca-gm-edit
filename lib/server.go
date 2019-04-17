4f43ed4fcfeab92e27961f0758bbc67898e3db4d
zhangqichao <zhangqichao@gjk365.com>
2019-04-17 15:55:15 +0800

    try to fix ca tls
    # Conflicts:
    #	lib/ca.go
    #	lib/caconfig.go
    #	lib/client.go
    #	lib/clientconfig.go
    #	lib/dbutil/dbutil.go
    #	lib/ldap/client.go
    #	lib/server.go
    #	lib/serverconfig.go

--- lib/server.go
+++ lib/server.go
@@ -545,7 +545,7 @@ func (s *Server) listenAndServe() (err error) {
 			return errors.New("Invalid client auth type provided")
 		}
 
-		var certPool *x509.CertPool
+		var certPool *sm2.CertPool
 		if authType != defaultClientAuth {
 			certPool, err = LoadPEMCertPool(c.TLS.ClientAuth.CertFiles)
 			if err != nil {
